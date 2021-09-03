293d9500b625651050d1a2bf2b88eaed38ab61f9
Saemi_hong <sstyle940104@gmail.com>
2021-09-03 14:11:17 +0900

    added events

--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Map, GeoJSON} from 'react-leaflet';
 import countries from './../data/countries.json';
 import 'leaflet/dist/leaflet.css'
@@ -7,6 +7,11 @@ import "./MyMap.css"
 
 const MyMap = () => {
     console.log(countries)
+
+const [color, setColor] = useState("#fff00")
+
+const colors = ["yellow", "red", "blue", "green", "purple"]
+
  const countryStyle={
      fillColor: "green",
      //fillOpacity no btwn 0-1
@@ -18,10 +23,33 @@ const MyMap = () => {
      dashArray :2
     }
 
+const colorChange=(e)=>{
+    setColor(e.target.value)
+}
+
+const onCountryClick=(e)=>{
+    console.log("clicked")
+}
+
+const onCountryMouseover=(event)=>{
+    event.target.setStyle({
+        color: color,
+        fillColor: "blue",
+})}
+
 const onEachCountry=(country, layer)=>{
     const countryName = country.properties.ADMIN
     console.log(countryName)
-    layer.bindPopup
+    //layer = shown on the screen
+    layer.bindPopup(countryName)
+//8. Random Country Colors With Opacity
+    layer.options.fillOpacity = Math.random()
+    layer.options.fillColor=colors[Math.floor(Math.random()*colors.length)]
+
+    layer.on({
+        click: onCountryClick,
+        mouseover : onCountryMouseover
+    })
 }
     return (
         <div>
@@ -29,7 +57,7 @@ const onEachCountry=(country, layer)=>{
             <Map style={{height: "80vh"}} zoom={5} center={[37.56948,126.97364]}>
                 <GeoJSON style={countryStyle} data={countries.features} onEachFeature={onEachCountry} />
             </Map>
-        
+            <input type="color" value={color} onChange={colorChange}/>
         </div>
     );
 };
@@ -41,3 +69,6 @@ export default MyMap;
 //3. style Map (background -> Map, color the countries -> GeoJSON)
 //4. display Pop-Up with onEachFeature
 //5. Layer.BindPopUp
+//6. Adding Events
+//7. REFACTORING
+//8. Adding a color picker input= color
